c13ebfe4c72f6629a4170f338a65b035745e0e58
Luciano <lucioggms210@gmail.com>
2023-12-22 00:22:59 -0600

    App sin estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { Dataquotes } from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.changeQuote = this.changeQuote.bind(this);
+        this.getRandomNumber = this.getRandomNumber.bind(this);
+
+        let initalIndex =  Math.floor(Math.random() * Dataquotes.length)
+        this.state = {
+            quotes: Dataquotes,
+            index: initalIndex,
+            text: Dataquotes[initalIndex].quote,
+            author: Dataquotes[initalIndex].author,
+        };
+    }
+
+    getRandomNumber(currentIndex = 0) {
+        let randomNumber;
+        do {
+            randomNumber = Math.floor(Math.random() * this.state.quotes.length);
+        } while (randomNumber === currentIndex);
+        return randomNumber;
+    }
+
+    changeQuote() {
+        const index = this.getRandomNumber(this.state.index);
+        console.log(index)
+        this.setState({
+            index: index,
+            text: this.state.quotes[index].quote,
+            author: this.state.quotes[index].author
+        });
+    }
+
+    render() {
+        return (
+            <div id={"quote-box"}>
+                <p id={"text"}>{this.state.text}</p>
+                <p id={"author"}>{this.state.author}</p>
+                <button id={"new-quote"} onClick={this.changeQuote}>New Quote</button>
+            </div>
+        );
+    }
 }
 
-export default App;
+
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const Dataquotes = [
+    {
+        author: "Albert Einstein",
+        quote: "La vida es muy peligrosa. No por las personas que hacen el mal, sino por las que se sientan a ver lo que pasa."
+    },
+    {
+        author: "Steve Jobs",
+        quote: "Tu trabajo va a llenar gran parte de tu vida, y la única manera de estar realmente satisfecho es hacer lo que crees es un gran trabajo."
+    },
+    {
+        author: "Nelson Mandela",
+        quote: "La educación es el arma más poderosa que puedes usar para cambiar el mundo."
+    },
+    {
+        author: "Maya Angelou",
+        quote: "Creer que eres lo suficientemente valioso es necesario para alcanzar tu potencial."
+    },
+    {
+        author: "Mahatma Gandhi",
+        quote: "Sé el cambio que quieres ver en el mundo."
+    }
+];
