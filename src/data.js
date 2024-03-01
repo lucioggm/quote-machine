0d52a975bf80efb0d8b471c583089445af58be8a
Luciano <lucioggm210@gmail.com>
2024-03-01 17:20:39 -0600

    arreglados algunos errores

--- src/data.js
+++ src/data.js
@@ -12,17 +12,37 @@ export const Dataquotes = [
         quote: "La educación es el arma más poderosa que puedes usar para cambiar el mundo."
     },
     {
-        author: "Maya Angelou",
-        quote: "Creer que eres lo suficientemente valioso es necesario para alcanzar tu potencial."
+        author: "Hollow knight",
+        quote: "...Sin mente para pensar.\n" +
+            "Sin voluntad que quebrar.\n" +
+            "Sin voz para gritar de sufrimiento.\n" +
+            "Nacido de Dios y el Vacío.\n" +
+            "Sellarás la luz cegadora que asola sus sueños.\n" +
+            "Eres el receptáculo.\n" +
+            "Eres el Hollow Knight"
     },
     {
         author: "Mahatma Gandhi",
         quote: "Sé el cambio que quieres ver en el mundo."
     },
     {
-        author: "Hormigas",
-        quote: "Sabas se la come"
+        author: "Andrew Ryan, Bioshock",
+        quote: "Ni Dioses, ni reyes, solo hombres."
+    },
+    {
+        author: "Dmitry Glukhovsky, Metro 2033" ,
+        quote: "Sólo hay una cosa que puede salvar a un hombre de la locura y es la incertidumbre."
+    },
+    {
+        author: " El fin de la muerte,Cixin Liu",
+        quote: "El universo es grande, pero la vida lo es aún más."
+    },
+    {
+        author: "Outer Wilds",
+        quote: "Esta canción es nueva para mí, pero es un honor para mí ser parte de ella."
     }
+
+
 ];
 
 export const Colores = ["#95b8f6","#add5fa","#f9d99a",
